00962a7b9b66137f555c51084c059f4f425bb51f
Dev Profile <kishore.rajkumar@hotmail.com>
2023-02-10 20:17:13 +0530

    added new projet

---
+++ template-spring-security/src/test/java/com/kishore/templatespringsecurity/TemplateSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kishore.templatespringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
