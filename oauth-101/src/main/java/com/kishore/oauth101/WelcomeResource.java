0bcaa20a4580d77a527c21f4edabd4a5c2d0efa3
Dev Profile <kishore.rajkumar@hotmail.com>
2023-02-19 14:38:45 +0530

    added new project

--- oauth-101/src/main/java/com/kishore/oauth101/WelcomeResource.java
+++ oauth-101/src/main/java/com/kishore/oauth101/WelcomeResource.java
@@ -1,12 +1,19 @@
 package com.kishore.oauth101;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeResource {
+	
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
 	@GetMapping({"/","welcome"})
-	public String welcome() {
+	public String welcome(Authentication authentication) {
+		logger.info(authentication.toString());
 		return "Welcome to Oauth2 101 Authentication Template application!";
 	}
 }
